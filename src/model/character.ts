0e8899613fa03c3a7df05bf6df37bb58b5c237c0
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-08-13 14:17:20 +0800

    [Create] Character model

---
+++ src/model/character.ts
@@ -0,0 +1,50 @@
+interface base_attribute {
+  hp: number;
+  mp: number;
+  str: number;
+  def: number;
+  int: number;
+  spd: number;
+  ap: number;
+  luck: number;
+  ms: number; // move speed
+  //vd: number // view distant
+}
+
+interface unit {
+  lv: number;
+  class: string;
+  name: string;
+  status: string;
+  base_attribute: base_attribute;
+  total_attribute: base_attribute;
+  attribute_limit: {
+    hp: number;
+    ap: number;
+    ms: number; // move speed
+    //vd: number // view distant
+  };
+}
+
+export type player = unit & {
+  equip: {
+    head: object;
+    body: object;
+    feet: object;
+    accasory: object;
+  };
+  bag: Array<object>;
+  exp: number;
+  attribute_limit: {
+    exp: number; // How much exp need to level up
+    bag: number; // How many items the bag can carry
+  };
+};
+
+export type enemy = unit & {
+  theme: string;
+  elite: boolean;
+  boss: boolean;
+  drop: Array<object>;
+  pattern: string;
+};
