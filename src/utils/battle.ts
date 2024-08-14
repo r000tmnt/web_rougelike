bc1f6a46807e91b40e3ae1f5ca5b4553913a2ee7
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-08-14 10:24:04 +0800

    [Create] Functions relate to battle logic

---
+++ src/utils/battle.ts
@@ -0,0 +1,44 @@
+import { player, enemy, base_attribute } from 'src/model/character';
+
+const grows = [0, 1, 3];
+
+export const levelUp = (data: player) => {
+  data.lv += 1;
+
+  for (const [key, value] of Object.entries(data.base_attribute)) {
+    data.base_attribute[key as keyof base_attribute] =
+      value + grows[Math.floor(Math.random() * grows.length)];
+  }
+
+  data.attribute_limit.hp = data.base_attribute.hp + data.add_attribute.hp;
+  data.attribute_limit.mp = data.base_attribute.mp + data.add_attribute.mp;
+  data.attribute_limit.exp += data.attribute_limit.exp * 1.5;
+  data.pt += 5;
+
+  return data;
+};
+
+export const setInitialStatus = (data: enemy, lv: number) => {
+  data.lv = lv;
+  const over = lv - 1;
+
+  for (const [key, value] of Object.entries(data.base_attribute)) {
+    data.base_attribute[key as keyof base_attribute] =
+      value + grows[Math.floor(Math.random() * grows.length)] * over;
+  }
+
+  data.attribute_limit.hp = data.base_attribute.hp + data.add_attribute.hp;
+  data.attribute_limit.mp = data.base_attribute.mp + data.add_attribute.mp;
+
+  return data;
+};
+
+export const becomeElite = (data: enemy) => {
+  data.elite = true;
+
+  for (const [key, value] of Object.entries(data.base_attribute)) {
+    data.base_attribute[key as keyof base_attribute] = value * 1.5;
+  }
+
+  return data;
+};
