7c5a648ae359c2a3e1bb13c1449ea05b37b785ea
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-08-14 10:20:44 +0800

    [Create] Skeleton base data

---
+++ src/data/skeleton.ts
@@ -0,0 +1,28 @@
+export default {
+  lv: 1,
+  name: 'Skeleton',
+  class: 'Undead',
+  status: 'Normal',
+  base_attribute: {
+    hp: 5,
+    mp: 1,
+    str: 3,
+    def: 2,
+    int: 1,
+    spd: 2,
+    luck: 2,
+  },
+  add_attribute: {
+    hp: 0,
+    mp: 0,
+    str: 0,
+    def: 0,
+    int: 0,
+    spd: 0,
+    luck: 0,
+  },
+  attribute_limit: {
+    hp: 5,
+    mp: 1,
+  },
+};
