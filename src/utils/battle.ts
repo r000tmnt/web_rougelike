6b9e56296f4e61b8922c1aa8b2b6ed933f84fbae
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-08-22 18:12:01 +0800

    [WIP] Calculate hit rate

--- src/utils/battle.ts
+++ src/utils/battle.ts
@@ -1,4 +1,3 @@
-import { base_attribute } from './../model/character';
 import { player, enemy, base_attribute } from 'src/model/character';
 
 const grows = [0, 1, 3];
@@ -46,24 +45,95 @@ export const becomeElite = (data: enemy) => {
 };
 
 export const calculateDamage = (attacker: any, defender: any, skill?: any) => {
+  const result = {
+    type: '',
+    value: 0,
+  };
+
   let baseDMG = attacker.base_attribute.str + attacker.add_attribute.str;
   const baseDEF = defender.base_attribute.def + defender.add_attribute.def;
 
-  if (Object.entries(attacker.equip.hand).length) {
-    // Alter baseDMG
+  if (attacker.equip) {
+    if (Object.entries(attacker.equip.hand).length) {
+      // Alter baseDMG
+    }
   }
 
-  if (Object.entries(skill).length) {
-    // Alter baseDMG
+  if (skill) {
+    if (Object.entries(skill).length) {
+      // Alter baseDMG
+    }
   }
 
   // Alter baseDEF if needed
 
-  if (baseDEF > baseDMG) {
-    baseDMG = 1; // Minimun damage
-  } else {
-    baseDMG -= baseDEF;
+  const rates = {
+    hitRate:
+      attacker.base_attribute.int +
+      attacker.add_attribute.int +
+      (attacker.base_attribute.int +
+        Math.floor(
+          attacker.base_attribute.int * (100 / attacker.base_attribute.spd)
+        )),
+    evadeRate:
+      defender.base_attribute.spd +
+      defender.add_attribute.spd +
+      (defender.base_attribute.spd +
+        Math.floor(defender.base_attribute * (100 / defender.base_attribute))),
+    critRate:
+      attacker.base_attribute.luck +
+      attacker.add_attribute.luck +
+      (attacker.base_attribute.int +
+        Math.floor(
+          attacker.base_attribute.int * (100 / attacker.base_attribute.luck)
+        )),
+  };
+
+  const totalRate = rates.hitRate + rates.evadeRate + rates.critRate;
+
+  for (const [key] of Object.entries(rates)) {
+    rates[key] = rates[key] / totalRate;
   }
 
-  return baseDMG;
+  // Object.keys(list).sort(function(a,b){return list[a]-list[b]})
+
+  Object.keys(rates).sort((a, b) => {
+    return rates[a] - rates[b];
+  });
+
+  console.log('rates :>>>', rates);
+
+  // Dice roll
+  const randomNumber = Math.random();
+
+  for (const [key, val] of Object.entries(rates)) {
+    if (val < randomNumber) {
+      if (key === 'hitRate') {
+        if (baseDEF > baseDMG) {
+          baseDMG = 1; // Minimun damage
+        } else {
+          baseDMG -= baseDEF;
+        }
+      }
+
+      if (key === 'evadeRate') {
+        baseDMG = 0;
+      }
+
+      if (key === 'critRate') {
+        if (baseDEF > baseDMG) {
+          baseDMG = Math.floor(1 * 1.5); // Minimun damage
+        } else {
+          baseDMG = Math.floor(baseDMG * 1.5) - baseDEF;
+        }
+      }
+
+      result.type = key;
+      result.value = baseDMG;
+
+      break;
+    }
+  }
+
+  return result;
 };
