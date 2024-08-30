f0e8b02cc740e36bce4eae505c388aa5bb109ebb
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-08-30 18:19:51 +0800

    [WIP] Show empty space

--- src/components/Player_inventory.vue
+++ src/components/Player_inventory.vue
@@ -1,23 +1,26 @@
 <template>
-  <section>
-    <table v-if="playerData.bag.length">
+  <section
+    class="q-pa-md"
+    :style="`font-size:${Math.floor(windowWidth / 100) * 2}px`"
+  >
+    INVENTORY
+
+    <table class="q-mx-auto" style="width: 80%">
       <tbody>
         <template
-          v-for="(row, rowIndex) in playerData.bag.length % 5 > 0
+          v-for="(row, rowIndex) in playerData.attribute_limit.bag % 5 > 0
             ? playerData.bag.length / 5 + 1
             : playerData.bag.length / 5"
           :key="row"
         >
           <tr>
             <td v-for="(col, colIndex) in 5" :key="col">
-              <div :data-index="colIndex + rowIndex * 5"></div>
+              <div class="grid" :data-index="colIndex + rowIndex * 5"></div>
             </td>
           </tr>
         </template>
       </tbody>
     </table>
-
-    <p v-else>EMPTY</p>
   </section>
 </template>
 
@@ -28,4 +31,6 @@ import { ref, computed } from 'vue';
 const gameStore = useGameStore();
 
 const playerData = computed(() => gameStore.getPlayer);
+
+const windowWidth = computed(() => gameStore.getWindowWidth);
 </script>
