d9f4903ef0fa92beca207884b5d45b09c54f64ee
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-08-30 15:59:29 +0800

    [Create] Player status component

---
+++ src/components/Player_status.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="flex">
+    <div>
+      <div class="status_header flex">
+        <span>NAME</span>
+        <span>
+          <div>LV</div>
+          <div>CLASS</div>
+        </span>
+      </div>
+    </div>
+    <div>
+      <!-- Equip -->
+      <ul>
+        <li v-for="(key, value) in playerData.equip" :key="key">
+          {{ key }} / {{ value }}
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { useGameStore } from '../stores/game';
+import { ref, computed } from 'vue';
+
+const gameStore = useGameStore();
+
+const playerData = computed(() => gameStore.getPlayer);
+</script>
