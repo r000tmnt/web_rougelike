2a8259c5b191957944a9b9f0e41ad76fdd36be57
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-08-30 15:59:13 +0800

    [Create] Player inventory component

---
+++ src/components/Player_inventory.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <table v-if="playerData.bag.length">
+      <tbody>
+        <template
+          v-for="(row, rowIndex) in playerData.bag.length % 5 > 0
+            ? playerData.bag.length / 5 + 1
+            : playerData.bag.length / 5"
+          :key="row"
+        >
+          <tr>
+            <td v-for="(col, colIndex) in 5" :key="col">
+              <div :data-index="colIndex + rowIndex * 5"></div>
+            </td>
+          </tr>
+        </template>
+      </tbody>
+    </table>
+
+    <p v-else>EMPTY</p>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { useGameStore } from '../stores/game';
+import { ref, computed } from 'vue';
+
+const gameStore = useGameStore();
+
+const playerData = computed(() => gameStore.getPlayer);
+</script>
