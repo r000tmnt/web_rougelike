a5f9c38906b33bfea61b069a6c20cb0137cbd711
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-09-02 16:47:17 +0800

    [Create] Item description as component

---
+++ src/components/Item_desc.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    id="desc"
+    class="absolute bg-dark grid"
+    :style="`width: ${
+      dynamicWidth * 3
+    }px;top:0;box-shadow: ${pixelatedBorder};${descElementPosition}`"
+  >
+    <img
+      class="q-mx-auto"
+      src=""
+      alt="image"
+      :style="`width: ${gameStore.tileSize}px; height: ${gameStore.tileSize}px`"
+    />
+    <ul>
+      <li>DESC</li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useGameStore } from '../stores/game';
+// import { ref, computed, onMounted } from 'vue';
+
+const gameStore = useGameStore();
+
+defineProps({
+  dynamicWidth: {
+    type: Number,
+    default: 0,
+  },
+  pixelatedBorder: {
+    type: String,
+    default: '',
+  },
+  descElementPosition: {
+    type: String,
+    default: '',
+  },
+});
+
+// TODO - Get item data
+</script>
