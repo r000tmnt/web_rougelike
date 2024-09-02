963156d05adbe6f6220580d920a33844684be58b
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-09-02 18:14:14 +0800

    [WIP] Defined types for various data

---
+++ src/data/types.ts
@@ -0,0 +1,17 @@
+export default {
+  scene: {
+    0: 'title',
+    1: 'hub',
+    2: 'dungeon',
+  },
+  item: {
+    0: 'head',
+    1: 'body',
+    2: 'hand',
+    3: 'feet',
+    4: 'accasory',
+    5: 'comsumable',
+    6: 'material',
+    7: 'special',
+  },
+};
