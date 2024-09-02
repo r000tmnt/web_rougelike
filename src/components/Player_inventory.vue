43c0e0f307bc7f12d0c5e883730aeb9faffa9d2e
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-09-02 09:41:27 +0800

    [Update] Display empty inventory

--- src/components/Player_inventory.vue
+++ src/components/Player_inventory.vue
@@ -5,32 +5,46 @@
   >
     INVENTORY
 
-    <table class="q-mx-auto" style="width: 80%">
-      <tbody>
-        <template
-          v-for="(row, rowIndex) in playerData.attribute_limit.bag % 5 > 0
-            ? playerData.bag.length / 5 + 1
-            : playerData.bag.length / 5"
-          :key="row"
-        >
-          <tr>
-            <td v-for="(col, colIndex) in 5" :key="col">
-              <div class="grid" :data-index="colIndex + rowIndex * 5"></div>
-            </td>
-          </tr>
-        </template>
-      </tbody>
-    </table>
+    <div class="hide-scrollbar scroll" style="height: 90%">
+      <table class="q-mx-auto" style="width: 80vw">
+        <tbody>
+          <template v-for="(row, rowIndex) in rows" :key="row">
+            <tr>
+              <td v-for="(col, colIndex) in 5" :key="col">
+                <div
+                  class="grid"
+                  :data-index="colIndex + rowIndex * 5"
+                  :style="`height: ${
+                    (Math.floor(windowWidth / 100) * 80) / 5
+                  }px`"
+                ></div>
+              </td>
+            </tr>
+          </template>
+        </tbody>
+      </table>
+    </div>
   </section>
 </template>
 
 <script setup lang="ts">
 import { useGameStore } from '../stores/game';
-import { ref, computed } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 
 const gameStore = useGameStore();
 
 const playerData = computed(() => gameStore.getPlayer);
 
 const windowWidth = computed(() => gameStore.getWindowWidth);
+
+const rows = ref<number>(0);
+
+onMounted(() => {
+  rows.value =
+    playerData.value.attribute_limit.bag % 5 > 0
+      ? playerData.value.attribute_limit.bag / 5 + 1
+      : playerData.value.attribute_limit.bag / 5;
+
+  console.log(rows.value);
+});
 </script>
