27b748d8efcf29a20633c192dc4b6822cc54fc43
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-09-04 18:11:37 +0800

    [Update] Keep the status part of the component

--- src/components/Player_status.vue
+++ src/components/Player_status.vue
@@ -1,63 +1,40 @@
 <template>
-  <section
-    class="row q-pa-md"
-    :style="`font-size:${Math.floor(windowWidth / 100) * 2}px`"
-  >
-    <div class="col-4">
-      <div class="status_header row">
-        <span class="col">NAME</span>
-        <span class="col">
-          <div>LV</div>
-          <div>CLASS</div>
-        </span>
+  <div class="status_header">
+    <div class="flex">
+      <div>NAME</div>
+      <div class="q-ml-auto">LV {{ playerData.lv }}</div>
+    </div>
+    <div class="flex">
+      <div>
+        <!-- CLASS -->
+        {{ playerData.class }}
       </div>
 
-      <div class="status">
-        <ul>
-          <li v-for="(key, value) in playerData.base_attribute" :key="value">
-            {{ value }}:
-            {{
-              playerData.base_attribute[value] + playerData.add_attribute[value]
-            }}
-            /
-            {{ playerData.attribute_limit[value] }}
-          </li>
-          <li>STAT: {{ playerData.status }}</li>
-          <li>
-            EXP: {{ playerData.exp }} / {{ playerData.attribute_limit.exp }}
-          </li>
-        </ul>
-      </div>
+      <div class="q-ml-auto">PT: {{ playerData.pt }}</div>
     </div>
-    <div class="col-8">
-      <!-- Equip -->
-      <ul>
-        <li v-for="(key, value) in playerData.equip" :key="value">
-          {{ value }}
-          <template
-            v-if="Object.entries(playerData.equip[value]).length"
-          ></template>
-          <template v-else> EMPTY </template>
-        </li>
-      </ul>
-    </div>
-  </section>
+  </div>
+
+  <div class="status">
+    <ul>
+      <li v-for="(key, value) in playerData.base_attribute" :key="value">
+        {{ value }}:
+        {{ playerData.base_attribute[value] + playerData.add_attribute[value] }}
+        /
+        {{ playerData.attribute_limit[value] }}
+      </li>
+      <li>STAT: {{ playerData.status }}</li>
+      <li>EXP: {{ playerData.exp }} / {{ playerData.attribute_limit.exp }}</li>
+    </ul>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { useGameStore } from '../stores/game';
-import { ref, computed } from 'vue';
+import { player } from '../model/character';
 
-const gameStore = useGameStore();
-
-const playerData = computed(() => gameStore.getPlayer);
-
-const windowWidth = computed(() => gameStore.getWindowWidth);
-
-// const dummyItem = ref({
-//   head: {
-//     id: '',
-//     name: ''
-//   }
-// })
+defineProps({
+  playerData: {
+    type: Object,
+    default: {} as player,
+  },
+});
 </script>
