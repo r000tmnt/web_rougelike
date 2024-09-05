75a8b27b3bab0e59f903aac043a6f08fa3fac929
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-09-05 17:29:10 +0800

    [Update] Listen to the custom event from mitt to update the health bar

--- src/components/Player_health_bar.vue
+++ src/components/Player_health_bar.vue
@@ -9,26 +9,24 @@
           calculateElementStyle() * 0.75
         )}px`"
       >
-        <div id="hp" ref="hp" class="meter">
-          <span>
-            {{ playerData?.base_attribute?.hp + playerData?.add_attribute?.hp }}
-            /
-            {{ playerData?.attribute_limit?.hp }}
-          </span>
-        </div>
+        <div id="hp" ref="hp" class="meter"></div>
+        <span>
+          {{ playerData?.total_attribute?.hp }}
+          /
+          {{ playerData?.attribute_limit?.hp }}
+        </span>
       </li>
       <li
         :style="`height:${calculateElementStyle()}px; font-size:${Math.floor(
           calculateElementStyle() * 0.75
         )}px`"
       >
-        <div id="mp" ref="mp" class="meter">
-          <span>
-            {{ playerData?.base_attribute?.mp + playerData?.add_attribute?.mp }}
-            /
-            {{ playerData?.attribute_limit?.mp }}
-          </span>
-        </div>
+        <div id="mp" ref="mp" class="meter"></div>
+        <span>
+          {{ playerData?.total_attribute?.mp }}
+          /
+          {{ playerData?.attribute_limit?.mp }}
+        </span>
       </li>
     </ul>
   </div>
@@ -36,9 +34,13 @@
 
 <script setup lang="ts">
 import { useGameStore } from '../stores/game';
-import { computed, watch, ref } from 'vue';
+// import { storeToRefs } from 'pinia';
+import { watch, ref, computed } from 'vue';
+import { player } from '../model/character';
 
 const gameStore = useGameStore();
+
+// const { player, windowWidth } = storeToRefs(gameStore);
 
 const playerData = computed(() => gameStore.getPlayer);
 
@@ -48,18 +50,8 @@ const hp = ref<HTMLDivElement | null>(null);
 
 const mp = ref<HTMLDivElement | null>(null);
 
-watch(
-  () => playerData,
-  (newData) => {
-    if (newData) {
-      setMeters();
-    }
-  },
-  { deep: true }
-);
-
 const getPercentage = (limit: number, current: number) => {
-  const each = Math.floor(limit / 100);
+  const each = limit / 100;
 
   return Math.floor(current / each);
 };
@@ -68,21 +60,51 @@ const calculateElementStyle = () => {
   return Math.floor(windowWidth.value / 50);
 };
 
-const setMeters = () => {
-  const { base_attribute, add_attribute, attribute_limit } = playerData.value;
+const setMeters = (data?: player) => {
+  const { total_attribute, attribute_limit } = data ? data : playerData.value;
   if (hp.value) {
     hp.value.style.width = `${getPercentage(
       attribute_limit.hp,
-      base_attribute.hp + add_attribute.hp
+      total_attribute.hp
     )}%`;
+
+    if (hp.value.parentNode) {
+      const textHolder = hp.value.parentNode.querySelector('span');
+
+      if (textHolder)
+        textHolder.innerText = `${total_attribute.hp} / ${attribute_limit.hp}`;
+    }
   }
   if (mp.value) {
     mp.value.style.width = `${getPercentage(
       attribute_limit.mp,
-      base_attribute.mp + add_attribute.mp
+      total_attribute.mp
     )}%`;
+
+    if (mp.value.parentNode) {
+      const textHolder = mp.value.parentNode.querySelector('span');
+
+      if (textHolder)
+        textHolder.innerText = `${total_attribute.mp} / ${attribute_limit.mp}`;
+    }
   }
 };
+
+// watch(
+//   () => player.value,
+//   (newData, oldData) => {
+//     console.log('newData', newData);
+//     console.log('oldData', oldData);
+//     if (newData) {
+//       setMeters();
+//     }
+//   },
+//   { deep: true }
+// );
+
+gameStore.emitter.on('player-attribute-change', (data: player) => {
+  setMeters(data);
+});
 </script>
 
 <style scoped lang="scss">
@@ -105,12 +127,14 @@ const setMeters = () => {
 
   ul {
     li {
-      div {
-        position: relative;
-        span {
-          position: absolute;
-          // margin: 0 auto;
-        }
+      background: gray;
+      position: relative;
+      div,
+      span {
+        position: absolute;
+      }
+      span {
+        z-index: 10;
       }
     }
   }
