c6f8ffc5818b690448c92e805417aa0727ac0a4d
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-09-06 16:42:25 +0800

    [Update] Set the equip list as a sortable element and clone the dragged item back to the position.

--- src/components/player_equip.vue
+++ src/components/player_equip.vue
@@ -10,6 +10,7 @@
         )}`"
         v-for="(key, value, index) in playerData.equip"
         :key="value"
+        :data-type="index"
         @mouseover="(e) => getItemPosition(e, playerData.equip[value], index)"
         @mouseleave="resetPosition"
       >
@@ -34,8 +35,9 @@
 <script setup lang="ts">
 import { useGameStore } from '../stores/game';
 import { item } from '../model/item';
-import { ref, computed } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 import Item_desc from './Item_desc.vue';
+import Sortable from 'sortablejs';
 
 const gameStore = useGameStore();
 
@@ -52,12 +54,12 @@ const hoveredItem = ref<item | object>({});
 const hoveredIndex = ref<number>(-1);
 
 const getItemPosition = (e: MouseEvent, item: item, index: number) => {
-  console.log(e);
+  // console.log(e);
 
   // Get hovered element position relative to viewport
   const target = e.target as HTMLDivElement;
   const el = target.getBoundingClientRect();
-  console.log('target element :>>>', el);
+  // console.log('target element :>>>', el);
 
   descElementPosition.value = `transform: translate(${
     el.left + dynamicWidth.value
@@ -72,4 +74,40 @@ const resetPosition = () => {
   hoveredIndex.value = -1;
   hoveredItem.value = {};
 };
+
+onMounted(() => {
+  const equipBlocks = document.getElementById('equip');
+
+  new Sortable(equipBlocks, {
+    sort: false,
+    group: {
+      name: 'shared',
+      put: true,
+      pull: (to, from) => {
+        console.log('to', to);
+        console.log('from equip', from);
+        return 'clone';
+      },
+      revertClone: true,
+    },
+    direction: function (evt, target, dragEl) {
+      console.log('equip target', target);
+      console.log('el drag to equip', dragEl);
+      return 'vertical';
+    },
+    onAdd: (e: any) => {
+      console.log('equip dropped ', e);
+    },
+
+    onRemove: (e: any) => {
+      console.log('equip removed ', e);
+      // Get the dragged col
+      const oldCol = e.oldIndex;
+      // Get the dropped item data
+      const itemData = Object.entries(playerData.value.equip)[oldCol];
+      // Remove the clone item
+      e.target.children[oldCol].innerHTML = `${itemData[0]} EMPTY`;
+    },
+  });
+});
 </script>
