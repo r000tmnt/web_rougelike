600ada9c6b06fd6add7bf44326871e4f0da3e45a
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-09-09 13:51:37 +0800

    [Update] Disable text highlight on mouse drag

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,24 @@
 <script setup lang="ts">
 import * as Phaser from 'phaser';
 window.Phaser = Phaser;
+
+const cancelSelection = () => {
+  const documentSelection = document.getSelection();
+  if (documentSelection) {
+    documentSelection.empty();
+  }
+
+  const windowSelection = window.getSelection();
+  if (windowSelection) {
+    windowSelection.removeAllRanges();
+  }
+};
+
+document.onmousemove = () => {
+  cancelSelection();
+};
+
+// document.onmouseup = () => {
+//   cancelSelection
+// }
 </script>
