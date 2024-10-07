666fc000b4c72405dc37ce3d32507a8b2143398f
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-10-07 14:57:04 +0800

    [Create] Game over screen

---
+++ src/components/Player_game_over.vue
@@ -0,0 +1,38 @@
+<template>
+  <section id="GameOver" class="absolute">
+    <div class="bg absolute fit"></div>
+    <div class="absolute fit text-center">
+      <h1 clss="text-bold">GAME OVER</h1>
+
+      <ul class="text-h4">
+        <li @click="restart">Try again</li>
+        <li @click="closeGame">Rest</li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import { useGameStore } from 'src/stores/game';
+
+const gameStore = useGameStore();
+
+const restart = () => {
+  gameStore.setGameOver(false);
+  gameStore.emitter.emit('reset');
+};
+
+const closeGame = () => {
+  window.close();
+};
+</script>
+
+<style scoped lang="scss">
+#GameOver {
+  background: transparent;
+  .bg {
+    background-color: #820000;
+    opacity: 0.5;
+  }
+}
+</style>
