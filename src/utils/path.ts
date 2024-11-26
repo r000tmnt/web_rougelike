249fa19ef27bac3845b1eb5eb0409b8cec2f0fac
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-11-26 15:44:08 +0800

    [Update] Support up to 8 direction

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -10,11 +10,24 @@ export const getPosition = (
   };
 };
 
+/**
+ *
+ * @param deg - A number to tell which direction the object is facing
+ * @returns 0: up, 1: up-right, 2: right, 3: right-down, 4: down, 5: left-down, 6: left, 7: left-up, -1: Not found
+ */
 export const getDirection = (deg: number) => {
-  if (deg <= -45 && deg >= -135) return 0; // up
-  if (deg <= 45 && deg >= -45) return 1; // right
-  if (deg <= 135 && deg >= 45) return 2; // down
-  if (deg <= -135 || deg >= 135) return 3; // left
+  if (deg === -90) return 0; // up
+  if (deg > -90 && deg < 0) return 1; // up right
+  if (deg === 0) return 2; // right
+  if (deg <= 45 && deg > 0) return 3; // right down
+  if (deg === 90) return 4; // down
+  if (deg <= 135 && deg > 90) return 5; // left down
+  if (deg === 180 || deg === -180) return 6; // left
+  if (deg > -180 && deg < -90) return 7; // left up
+  // if (deg <= -45 && deg >= -135) return 0;
+  // if (deg <= 45 && deg >= -45) return 1; // right
+  // if (deg <= 135 && deg >= 45) return 2; // down
+  // if (deg <= -135 || deg >= 135) return 3; // left
   else return -1; // Not found
 };
 
