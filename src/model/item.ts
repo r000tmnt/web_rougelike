e7bebd80a51cd8869aa1d3ad8057d6ab3329fbf8
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2025-02-14 15:56:24 +0800

    [Update] Define item type and item rarity type

--- src/model/item.ts
+++ src/model/item.ts
@@ -1,15 +1,65 @@
+type ItemTypes =
+  | 'head'
+  | 'body'
+  | 'hand'
+  | 'feet'
+  | 'accessory'
+  | 'consumable'
+  | 'material'
+  | 'special';
+
+type RarityTypes = 'common' | 'advance' | 'rare' | 'unique' | 'legendary';
+
+type rarity = {
+  type: Partial<RarityTypes>;
+  color: string;
+};
+
+export const ITEM_TYPES: Record<number, Partial<ItemTypes>> = {
+  0: 'head',
+  1: 'body',
+  2: 'hand',
+  3: 'feet',
+  4: 'accessory',
+  5: 'consumable',
+  6: 'material',
+  7: 'special',
+};
+
+export const RARITY_COLORS: Record<number, rarity> = {
+  0: {
+    type: 'common',
+    color: '#ffffff', // White
+  },
+  1: {
+    type: 'advance',
+    color: '#1eff00', // Green
+  },
+  2: {
+    type: 'rare',
+    color: '#0070dd', // Blue
+  },
+  3: {
+    type: 'unique',
+    color: '#a335ee', // Purple
+  },
+  4: {
+    type: 'legendary',
+    color: '#ff8000', // Orange
+  },
+};
 export interface item {
   id: string;
   name: string;
   type: number;
   rarity: number;
   desc: string;
-  image: string; // Path of the image
   modifier: string;
   equip?: boolean;
   effect?: modifier;
   amount: number;
-  index?: number;
+  limit: number; // The number of the item can be stack up
+  index?: number; // Path of the image
 }
 
 export interface modifier {
