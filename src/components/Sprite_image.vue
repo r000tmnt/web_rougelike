126d9261dcd9408f692408b249fcbaff7c6d59ba
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2025-02-20 15:25:25 +0800

    [Create] Sprite image component for inventory

---
+++ src/components/Sprite_image.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="sprite" :style="spriteStyle"></div>
+</template>
+
+<script lang="ts" setup>
+// import { useGameStore } from 'src/stores/game';
+import { computed } from 'vue';
+
+const props = defineProps({
+  index: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const spriteStyle = computed(() => {
+  const position = props.index * 24;
+  return {
+    backgroundImage: 'url(/assets/demo_item.png)',
+    backgroundPosition: `-${position}px -${position}px`,
+    width: '24px',
+    height: '24px',
+    backgroundSize: 'auto',
+    margin: '0 auto',
+  };
+});
+</script>
