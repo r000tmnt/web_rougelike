cfa44741f5eb5146a579815eabcd78d35d41fd93
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2025-03-07 16:42:20 +0800

    [Update] Expand the component to have it's own drag & drop event

--- src/components/Sprite_image.vue
+++ src/components/Sprite_image.vue
@@ -1,27 +1,74 @@
 <template>
-  <div class="sprite" :style="spriteStyle"></div>
+  <div
+    v-show="index >= 0"
+    class="sprite"
+    :style="`background-position: ${spriteStyle}; position: ${spritePosition}; ${draggingStyle}`"
+  ></div>
 </template>
 
 <script lang="ts" setup>
 // import { useGameStore } from 'src/stores/game';
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 
 const props = defineProps({
   index: {
     type: Number,
-    default: 0,
+    default: -1,
   },
 });
 
+const emit = defineEmits(['dragEnd']);
+
+const spritePosition = ref<string>('relative');
+
+const draggingPosition = ref({
+  x: 0,
+  y: 0,
+});
+
+const draggingStyle = computed(() =>
+  spritePosition.value === 'absolute'
+    ? `left: ${draggingPosition.value.x}px; top: ${draggingPosition.value.y}px;`
+    : ''
+);
+
+const onDrag = (e: MouseEvent, signal: boolean) => {
+  spritePosition.value = signal ? 'absolute' : 'relative';
+  onMove(e);
+  if (signal) {
+    window.addEventListener('mousemove', onMove);
+    window.addEventListener('mouseup', onDrop);
+  }
+};
+
+const onMove = (e: MouseEvent) => {
+  draggingPosition.value.x = e.pageX - 24;
+  draggingPosition.value.y = e.pageY - 24;
+};
+
+const onDrop = (e: MouseEvent) => {
+  window.removeEventListener('mousemove', onMove);
+  window.removeEventListener('mouseup', onDrop);
+  onDrag(e, false);
+  emit('dragEnd');
+};
+
 const spriteStyle = computed(() => {
   const position = props.index * 24;
-  return {
-    backgroundImage: 'url(/assets/demo_item.png)',
-    backgroundPosition: `-${position}px -${position}px`,
-    width: '24px',
-    height: '24px',
-    backgroundSize: 'auto',
-    margin: '0 auto',
-  };
+  return `-${position}px -${position}px`;
+});
+
+defineExpose({
+  onDrag,
 });
 </script>
+
+<style scpoed>
+.sprite {
+  background-image: url('/assets/demo_item.png');
+  width: 24px;
+  height: 24px;
+  background-size: auto;
+  margin: 0 auto;
+}
+</style>
