e29aaff5f2de90d42b4f4cd40989ed02136a943f
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2025-04-16 16:44:31 +0800

    [Create] Loot table collector file

---
+++ src/data/lootTable.ts
@@ -0,0 +1,23 @@
+import accessory from './item/accessory.json';
+import body from './item/body.json';
+import cards from './item/cards.json';
+import currencies from './item/currencies.json';
+import feet from './item/feet.json';
+import hand from './item/hand.json';
+import head from './item/head.json';
+import materials from './item/materials.json';
+import potions from './item/potions.json';
+
+const lootTable = {
+  accessory,
+  body,
+  cards,
+  currencies,
+  feet,
+  hand,
+  head,
+  materials,
+  potions,
+};
+
+export default lootTable;
